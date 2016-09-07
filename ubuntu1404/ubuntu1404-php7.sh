c1a8e35c44ad9bbf99d3c4949dfffd52255ee972
Cheng-Chung Chen <chengchungchen@talk2yam.com>
2016-09-07 14:49:15 +0800

    Editing php7.0

--- ubuntu1404/ubuntu1404-php7.sh
+++ ubuntu1404/ubuntu1404-php7.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-# This script is to install Nginx and PHP to Ubuntu12.04 & Ubuntu14.04
+# This script is to install PHP 7.0 to Ubuntu12.04 & Ubuntu14.04
 # WARNINGS! This only for Ubuntu!
 
+# Adding a PPA for PHP7.0 Packages
+add-apt-repository ppa:ondrej/php
+apt-get update
+
 # Install Nginx and PHP7
-#apt-get install -y php5 php5-cli php5-common php5-curl php5-dev php5-fpm php5-gd php5-mcrypt php5-memcache php5-mysqlnd php5-xmlrpc php5-sqlite php5-sybase php-pear php-db php-gettext
-apt-get install -y php7.0-fpm php7.0-gd php7.0-json php7.0-mbstring php7.0-mcrypt php7.0-mysql php7.0-opcache php7.0-readline
+apt-get install -y php7.0-fpm php7.0-cli php7.0-gd php7.0-json php7.0-mbstring php7.0-mcrypt php7.0-mysql php7.0-opcache php7.0-readline php7.0-xml
 
 # Setting the PHP5
 #mkdir -p /home/log
@@ -28,9 +31,6 @@ sed -i 's/upload_max_filesize = 2M/upload_max_filesize = 64M/g' /etc/php/7.0/fpm
 sed -i 's/;date.timezone =/date.timezone = "Asia\/Taipei"/g' /etc/php/7.0/fpm/php.ini
 
 # Setting the php-fpm.conf
-if [ ! -f /etc/php/7.0/fpm/php-fpm.conf.default ]; then
-	cp /etc/php/7.0/fpm/php-fpm.conf /etc/php/7.0/fpm/php-fpm.conf.default
-fi
 sed -i 's/;emergency_restart_threshold = 0/emergency_restart_threshold = 10/g' /etc/php/7.0/fpm/php-fpm.conf
 sed -i 's/;emergency_restart_interval = 0/emergency_restart_interval = 1m/g' /etc/php/7.0/fpm/php-fpm.conf
 sed -i 's/;process_control_timeout = 0/process_control_timeout = 10s/g' /etc/php/7.0/fpm/php-fpm.conf
@@ -42,13 +42,11 @@ fi
 #sed -i 's/www-data/nginx/g' /etc/php5/fpm/pool.d/www.conf
 
 # Setting logrotate
-#mkdir -p /root/defaultfile
-#if [ -f /etc/logroate.d/nginx ]; then
-#	mv /etc/logroate.d/nginx /root/defaultfile/etc.logroate.d.nginx
-#fi
-#cp etc.logrotate.d.nginx /etc/logrotate.d/nginx
+if [ ! -d /root/defaultfile ]; then
+    mkdir -p /root/defaultfile
+fi
 
-#if [ -f /etc/logroate.d/php5-fpm ]; then
-#	mv /etc/logroate.d/php5-fpm /root/defaultfile/etc.logroate.d.php5-fpm
-#fi
-#cp etc.logrotate.d.php5-fpm /etc/logrotate.d/php5-fpm
+if [ -f /etc/logroate.d/php7.0-fpm ]; then
+	mv /etc/logroate.d/php7.0-fpm /root/defaultfile/etc.logroate.d.php7.0-fpm
+fi
+cp etc.logrotate.d.php7.0-fpm /etc/logrotate.d/php7.0-fpm
